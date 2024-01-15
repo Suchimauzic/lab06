303b2a099e116f3d26e23c02c0bc92fb817a5bce
Suchimauzic <sergdog03@gmail.com>
2024-01-15 13:25:49 +0300

    lab06

---
+++ app/src/main/java/g313/gusev/lab06/MainActivity.java
@@ -0,0 +1,27 @@
+package g313.gusev.lab06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnNew_Click(View v) {
+
+    }
+
+    public void btnEdit_Click(View v) {
+
+    }
+
+    public void btnDelete_Click(View v) {
+
+    }
+}
